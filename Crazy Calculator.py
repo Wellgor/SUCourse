8ae2b74e39d550c48ce6b1b1e58b369c123ffd35
Pavel Horbatiuk <75274142+Wellgor@users.noreply.github.com>
2020-12-10 18:44:10 +0200

    Crazy Calculator

---
+++ Crazy Calculator.py
@@ -0,0 +1,21 @@
+ban=1
+bin=2
+bon=3
+glo=4
+gloo=5
+gun=6
+gym=7
+gum=8
+doom=9
+print("Crazy Calculator")
+print("Hello, it's me, your best friend!")
+print('I know these numbers: ban,bin,bon,glo,gloo,gun,gym,gum,doom')
+print('Try to do mathematical actions with them!')
+print('Good luck!')
+
+
+print(gun+gym)
+print(gum*gun)
+print(gym*gum)
+print(gun+gum)
+print(gloo+gun)
